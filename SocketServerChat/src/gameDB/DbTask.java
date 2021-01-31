86f3adeaedd33d1c68be9d7e1edc706f331d0d9c
Atef <mahmoudbenatef@gmail.com>
2021-02-01 00:56:49 +0200

    remove all comments and solve draw between players

--- SocketServerChat/src/gameDB/DbTask.java
+++ SocketServerChat/src/gameDB/DbTask.java
@@ -19,7 +19,6 @@ import socketserverchat.Classes.Player;
  *
  * @author atef
  */
- 
 public class DbTask {
 
     static Connection con = null;
@@ -27,22 +26,17 @@ public class DbTask {
     public static void defineConnection() {
 
         try {
-            System.out.println("Defining connection");
             //String url = "jdbc:mysql://localhost:3306/game";
             //String user = "atef";
             //String password = "Iwasbornin1998$";
             // con = DriverManager.getConnection(url, user, password);
-            if(con == null)
-            {
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/game?user=atef&password=Iwasbornin1998$");
+            if (con == null) {
+                con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/game?user=atef&password=Iwasbornin1998$");
             }
-            
+
         } catch (Exception e) {
-            System.out.println("not connected");
         }
     }
-
-   
 
     public static Player getPerson(Player p) {
 
@@ -64,18 +58,16 @@ public class DbTask {
             }
 
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
-        
+
         if (p != null) {
             setActive(p.getId());
         }
-        
+
         return p;
     }
-    
-    public static void setActive (int personId) {
-        System.out.println("updated");
+
+    public static void setActive(int personId) {
         try {
             PreparedStatement pst = con.prepareStatement("UPDATE players SET flag = ?  where id = ?");
             pst.setInt(1, 1);
@@ -83,12 +75,10 @@ public class DbTask {
             pst.executeUpdate();
 
         } catch (Exception e) {
-            System.out.println("error");
         }
     }
-    
-     public static int register(Player p) {
-        System.out.println("here in insert");
+
+    public static int register(Player p) {
         String insertTableSQL = "INSERT INTO players"
                 + "(username,password,nickname,points,flag) VALUES"
                 + "(?,?,?,?,?)";
@@ -103,40 +93,35 @@ public class DbTask {
             return DbTask.getID(p.getUsername());
 
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
             return -1;
         }
     }
-     
+
     public static int getID(String userName) {
         String sql = "SELECT id FROM players WHERE username = ?";
         try (
                 PreparedStatement pstmt = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
             pstmt.setString(1, userName);
             ResultSet resultSet = pstmt.executeQuery();
-            if (resultSet.next())
-            {
+            if (resultSet.next()) {
                 return resultSet.getInt(1);
             }
 
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
         return -1;
     }
-     
+
     public static ArrayList<Player> getAll(String username) {
         defineConnection();
-        System.out.println("my connection : "+con);
-        //System.out.println("getAll "+id);
         ResultSet resultSet = null;
         ArrayList<Player> players = new ArrayList<>();
         String sql = "select * from players WHERE username != ? ORDER BY flag desc, points desc";
         try (
                 PreparedStatement pstmt = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE)) {
-                pstmt.setString(1, username);
-                resultSet = pstmt.executeQuery();
-                
+            pstmt.setString(1, username);
+            resultSet = pstmt.executeQuery();
+
             while (resultSet.next()) {
                 Player p = new Player();
                 p.setId(resultSet.getInt(1));
@@ -150,18 +135,14 @@ public class DbTask {
 
             }
             for (Player p : players) {
-                System.out.println(p.getUsername());
             }
 
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
-        System.out.println(players);
         return players;
     }
-    
+
     public static void updateOffLine(String username) {
-        System.out.println("updated");
         try {
             PreparedStatement pst = con.prepareStatement("UPDATE players SET flag = ?  where username = ?");
             pst.setInt(1, 0);
@@ -169,35 +150,32 @@ public class DbTask {
             pst.executeUpdate();
 
         } catch (Exception e) {
-            System.out.println("error");
         }
     }
-    
-     public static void updateScore(String winner){
+
+    public static void updateScore(String winner) {
         try {
             PreparedStatement pst = con.prepareStatement("UPDATE players SET points = points+10  where username = ?");
             pst.setString(1, winner);
             pst.executeUpdate();
-            System.out.println("did it work dada");
 
         } catch (Exception e) {
-            System.out.println("error");
         }
 
     }
-    public static void saveMap(String mapJson,String username){
+
+    public static void saveMap(String mapJson, String username) {
         try {
             PreparedStatement pst = con.prepareStatement("UPDATE players SET map = ?  where username = ?");
             pst.setString(1, mapJson);
-            pst.setString(2,username);
+            pst.setString(2, username);
             pst.executeUpdate();
-            System.out.println("did it work dada");
 
         } catch (Exception e) {
-            System.out.println("error");
         }
     }
-    public static String getMap(String userName){
+
+    public static String getMap(String userName) {
         String sql = "SELECT map FROM players WHERE username = ?";
         String map = null;
         try (
@@ -205,14 +183,12 @@ public class DbTask {
             pstmt.setString(1, userName);
 
             ResultSet resultSet = pstmt.executeQuery();
-            if(resultSet.next()) {
-                 map = resultSet.getString("map");
+            if (resultSet.next()) {
+                map = resultSet.getString("map");
             }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
-       return map;
+        return map;
     }
-    
 
 }
--- SocketServerChat/src/socketserverchat/ServerGUI.java
+++ SocketServerChat/src/socketserverchat/ServerGUI.java
@@ -46,28 +46,27 @@ import socketserverchat.Classes.Player;
  * @author atef
  */
 public class ServerGUI extends Application {
-    
+
     private boolean firstStartServerFlag = false;
     private Thread serverThread;
     private SocketServerChat startServer;
     Thread updatePlayerThread;
     ListView<String> listView;
-    
+
     @Override
     public void init() {
-        
+
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+
         Label label = new Label("Educational qualification:");
 
         //list View for educational qualification
         //  ObservableList<String> names = FXCollections.observableArrayList("Engineering", "MCA", "MBA", "Graduation", "MTECH", "Mphil", "Phd");
         listView = new ListView<String>();
-          
-                       
+
         listView.setPrefSize(200, 520);
         //Creating the layout
         VBox layout = new VBox(10);
@@ -80,102 +79,71 @@ public class ServerGUI extends Application {
         int timeInterval = 1000;
         timer.schedule(new TimerTask() {
             int counter = 0;
-            
+
             @Override
             public void run() {
                 counter++;
-                /*             
-                if (!SocketServerChat.isUpdatedUser) {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            // do some code 
-                            System.out.println(SocketServerChat.allPlayers.size());
-                            SocketServerChat.isUpdatedUser = false;
-                        }
-                    });
-                } else {
-                 */
-                System.out.println(SocketServerChat.allPlayers.size());
+
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
-                        
+
                         if (listView != null) {
-                            System.out.println("combo box update is not empty");
                             listView.getItems().clear();
-                            
+
                             for (Player pp : DbTask.getAll("")) {
                                 listView.getItems().add(
                                         pp.getUsername() + "," + pp.getPoints() + "," + pp.getFlag()
                                 );
                             }
 
-                            //  listView.setValue("please choose one player");
-                            // Set the CellFactory property
                             listView.setCellFactory(new ShapeCellFactory());
-                            // Set the ButtonCell property
-                            //listView.setButtonCell(new ShapeCell());
-                            ////////////////////////////////////////////////
-                            //  listView.setValue("please choose one player");
-                            // listView.setId(("combo-box"));
-                            
+
                         }
-                        
-                        System.out.println(SocketServerChat.allPlayers.size());
+
                         SocketServerChat.isUpdatedUser = false;
                     }
                 });
-                
+
                 for (Player pp : SocketServerChat.allPlayers) {
-                    System.out.println(pp.getUsername() + " , " + pp.getFlag());
                 }
 //                }
 
             }
         }, begin, timeInterval);
-        
+
         BorderPane root = new BorderPane();
         root.setId("second-pane");
         Button startButton = new Button("Start");
-        
+
         startButton.setId("startbutton");
-        
+
         Button stopButton = new Button("Stop");
-        
+
         stopButton.setId("stopbutton");
         HBox buttons = new HBox(30, startButton, stopButton);
         buttons.setAlignment(Pos.CENTER);
-        
+
         startButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                 Platform.runLater(new Runnable() {
+                Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
-                        
+
                         if (listView != null) {
-                            System.out.println("combo box update is not empty");
                             listView.getItems().clear();
-                            
+
                             for (Player pp : DbTask.getAll("")) {
                                 listView.getItems().add(
                                         pp.getUsername() + "," + pp.getPoints() + "," + pp.getFlag()
                                 );
                             }
 
-                            //  listView.setValue("please choose one player");
-                            // Set the CellFactory property
                             listView.setCellFactory(new ShapeCellFactory());
-                            // Set the ButtonCell property
-                            //listView.setButtonCell(new ShapeCell());
-                            ////////////////////////////////////////////////
-                            //  listView.setValue("please choose one player");
-                            // listView.setId(("combo-box"));
-                            
+
                         }
-                        
-                        System.out.println(SocketServerChat.allPlayers.size());
+
                         SocketServerChat.isUpdatedUser = false;
                     }
                 });
@@ -184,22 +152,20 @@ public class ServerGUI extends Application {
                     ServerSocketThread.setPressedButton("start");
                     ServerSocketThread serverStart = new ServerSocketThread();
                     SocketServerChat.resumeServerSocket();
-                    System.out.println("Thread: " + serverThread);
                 }
             }
         });
-        
+
         stopButton.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 firstStartServerFlag = false;
-                System.out.println("Thread suspended");
                 ServerSocketThread.setPressedButton("stop");
                 SocketServerChat.stopServerSocket();
             }
         });
-        
+
         root.setCenter(buttons);
         root.setRight(layout);
         Scene scene = new Scene(root, 800, 500);
@@ -208,7 +174,7 @@ public class ServerGUI extends Application {
 //        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-        
+
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             public void handle(WindowEvent we) {
                 System.exit(0);
@@ -222,36 +188,35 @@ public class ServerGUI extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
 
 class ServerSocketThread extends Thread {
-    
+
     private static SocketServerChat startServer;
     private static String pressedButton;
-    
+
     public static String getPressedButton() {
         return pressedButton;
     }
-    
+
     public static SocketServerChat getStartServer() {
         return startServer;
     }
-    
+
     public static void setPressedButton(String buttonStatus) {
         pressedButton = buttonStatus;
     }
-    
+
     public ServerSocketThread() {
         this.start();
     }
-    
+
     @Override
     public void run() {
         try {
-            
+
             startServer = new SocketServerChat();
-            System.out.println("port: " + startServer.getServerSocket());
         } catch (IOException ex) {
             Logger.getLogger(ServerSocketThread.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -260,11 +225,11 @@ class ServerSocketThread extends Thread {
 
 //To edit combobox shape
 class ShapeCell extends ListCell<String> {
-    
+
     @Override
     public void updateItem(String item, boolean empty) {
         super.updateItem(item, empty);
-        
+
         if (empty) {
             setText(null);
             setGraphic(null);
@@ -274,11 +239,11 @@ class ShapeCell extends ListCell<String> {
             setGraphic(cellShape);
         }
     }
-    
+
     public GridPane getShape(String shapeType) {
         GridPane shape = null;
         Circle status;
-        
+
         if (shapeType != null) {
             String[] inputItemsSplit = shapeType.split(",");
             if (Integer.valueOf(inputItemsSplit[2]) == 1) {
@@ -292,13 +257,13 @@ class ShapeCell extends ListCell<String> {
             shape.add(playerNameStatus, 0, 0);
             shape.add(playerScore, 0, 1);
         }
-        
+
         return shape;
     }
 }
 
 class ShapeCellFactory implements Callback<ListView<String>, ListCell<String>> {
-    
+
     @Override
     public ListCell<String> call(ListView<String> listview) {
         return new ShapeCell();
